e71b73bcd2ecf3fa139f819919ad83589c81b53c
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2014-04-17 23:56:49 -0700

    Removed satisfy constraint method from constraint class to cloth class

--- cloth_4_3_compute/cloth_4_3_compute.h
+++ cloth_4_3_compute/cloth_4_3_compute.h
@@ -161,7 +161,7 @@ namespace cloth_4_3_compute
 
     class Constraint
     {
-    private:
+    public:
         float rest_distance; // the length between particle p1 and p2 in rest configuration
 
     public:
@@ -174,17 +174,6 @@ namespace cloth_4_3_compute
             rest_distance = length(vec);
         }
 
-        /* This is one of the important methods, where a single constraint between two particles p1 and p2 is solved
-        the method is called by Cloth.time_step() many times per frame*/
-        void satisfyConstraint()
-        {
-            vec3 p1_to_p2 = p2->getPos() - p1->getPos(); // vector from p1 to p2
-            float current_distance = length(p1_to_p2); // current distance between p1 and p2
-            vec3 correctionVector = p1_to_p2*(1 - rest_distance / current_distance); // The offset vector that could moves p1 into a distance of rest_distance to p2
-            vec3 correctionVectorHalf = correctionVector*0.5f; // Lets make it half that length, so that we can move BOTH p1 and p2.
-            p1->offsetPos(correctionVectorHalf); // correctionVectorHalf is pointing from p1 to p2, so the length should move p1 half the length needed to satisfy the constraint.
-            p2->offsetPos(-correctionVectorHalf); // we must move p2 the negative direction of correctionVectorHalf since it points from p2 to p1, and not p1 to p2.	
-        }
     };
 
     struct Cloth
@@ -212,6 +201,18 @@ namespace cloth_4_3_compute
             uint p2Idx = m_particleIndex[p2];
             constraints.push_back(Constraint(p1, p2, p1Idx, p2Idx));
         }
+
+		/* This is one of the important methods, where a single constraint between two particles p1 and p2 is solved
+		the method is called by Cloth.time_step() many times per frame*/
+		void satisfyConstraint(Constraint* pConstraint)
+		{
+			vec3 p1_to_p2 = pConstraint->p2->getPos() - pConstraint->p1->getPos(); // vector from p1 to p2
+			float current_distance = length(p1_to_p2); // current distance between p1 and p2
+			vec3 correctionVector = p1_to_p2*(1 - pConstraint->rest_distance / current_distance); // The offset vector that could moves p1 into a distance of rest_distance to p2
+			vec3 correctionVectorHalf = correctionVector*0.5f; // Lets make it half that length, so that we can move BOTH p1 and p2.
+			pConstraint->p1->offsetPos(correctionVectorHalf); // correctionVectorHalf is pointing from p1 to p2, so the length should move p1 half the length needed to satisfy the constraint.
+			pConstraint->p2->offsetPos(-correctionVectorHalf); // we must move p2 the negative direction of correctionVectorHalf since it points from p2 to p1, and not p1 to p2.	
+		}
 
 
         /* A private method used by drawShaded() and addWindForcesForTriangle() to retrieve the
@@ -440,7 +441,7 @@ namespace cloth_4_3_compute
             {
                 for (constraint = constraints.begin(); constraint != constraints.end(); constraint++)
                 {
-                    (*constraint).satisfyConstraint(); // satisfy constraint.
+					satisfyConstraint(&(*constraint)); // satisfy constraint.
                 }
             }
 
